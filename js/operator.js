4dc0d0b0ad3742ec181cf77f01865d91a90422aa
Anurodh Prasain <anurodhprasain0011@gmail.com>
2024-12-31 19:23:08 +0545

    Main es6 operators

--- js/operator.js
+++ js/operator.js
@@ -19,17 +19,6 @@ console.log(productName) // Iphone
 console.log(price) // 1000
 console.log(brand) // Apple
 
-
-//Array Destructuring
-let numbers = [10, 20, 30, 40, 50]
-let [num1, num2, num3, num4, num5] = numbers
-console.log(num1) // 10
-console.log(num2) // 20
-console.log(num3) // 30
-console.log(num4) // 40
-console.log(num5) // 50
-
-
 // let obj1 = {
 //     name: "John",
 //     age: 30
@@ -53,8 +42,11 @@ obj2.name = "New Name"
 console.log(obj1.name) // New Name
 
 
+//Spread Operator
+
 let config = {
-    name: "John"
+    name: "John",
+    weight: 70
 }
 
 
@@ -63,3 +55,6 @@ config = {
     ...config,
     age: 30
 }
+
+//rest operator
+let { name, ...newObj } = config
