41d68387051d175ff79ecfef843e1a02d7252212
Anurodh Prasain <anurodhprasain0011@gmail.com>
2025-01-02 23:19:39 +0545

    control

--- js/control.js
+++ js/control.js
@@ -43,31 +43,3 @@ let dayNum = 3
 //     console.log("Invalid Day")
 // }
 
-switch (dayNum) {
-    case 1:
-        console.log("Sunday")
-        break;
-    case 2:
-        console.log("Monday")
-        break;
-    case 3:
-        console.log("Tuesday")
-        break;
-    case 4:
-        console.log("Wednesday")
-        break;
-    case 5:
-        console.log("Thursday")
-        break;
-    case 6:
-        console.log("Friday")
-        break;
-    case 7:
-        console.log("Saturday")
-        break;
-    default:
-        console.log("Invalid Day")
-        break;
-}
-
-
