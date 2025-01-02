72264fe29091de7fcefb74c2c9d8044a2881442f
Anurodh Prasain <anurodhprasain0011@gmail.com>
2025-01-02 21:54:20 +0545

    Practicing if_else

---
+++ js/practice/if_else.js
@@ -0,0 +1,22 @@
+// 1. Write a program that takes a number as input and checks if the number is positive, negative, or zero.
+// let num = -7
+// console.log(num > 0 ? "Positive" : num === 0 ? "Zero" : "Negative")
+
+// 2. Even or Odd
+// Write a program that checks if a number is even or odd.
+// let num = -20
+// console.log(num % 2 === 0 ? "Even" : "Odd")
+
+
+// 3. Vowel or Consonant
+// Write a program that checks whether a given character is a vowel or a consonant.
+// let char = "b"
+// console.log((char === "a" || char === "e" || char === "i" || char === "o" || char === "u") ? "Vowel" : "Consonent")
+
+
+// 4. Age Category
+// Write a program that checks the age of a person and categorizes them as:
+
+// Under 18: Minor
+// Between 18 and 65: Adult
+// Above 65: Senior
