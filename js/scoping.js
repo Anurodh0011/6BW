bf2cac406161d2da5acc6e682f04b92fd65eb374
Anurodh Prasain <anurodhprasain0011@gmail.com>
2025-01-02 19:19:55 +0545

    scoping

--- js/scoping.js
+++ js/scoping.js
@@ -1,8 +1,31 @@
-console.log("Hello, World!")
-var x = 5;
-console.log(x)
+// Scoping
+
+// variable declared using var is global whether it is declared inside a block or outside a block.
+
+// var x = 5;
+// console.log(x)
+// {
+//     var x = 10;
+//     console.log(x)
+// }
+// console.log(x)
+
+
+// variable declared using let is block scoped. It is only accessible within the block it is declared.
+let x = 5;
+console.log(x)   //5
 {
-    var x = 10;
-    console.log(x)
+    let x = 10;
+    console.log(x)     //10
 }
-console.log(x)
+console.log(x)  //5
+
+
+// variable declared using let is block scoped. It is only accessible within the block it is declared.
+let y = 5;
+console.log(y)      //5
+{
+    y = 10;
+    console.log(y)   //10
+}
+console.log(y)      //10  
