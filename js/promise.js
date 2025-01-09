926e553da506c90e0128c74557f949906f6bb274
Anurodh Prasain <anurodhprasain0011@gmail.com>
2025-01-09 19:24:10 +0545

    promise question

---
+++ js/promise.js
@@ -0,0 +1,4 @@
+// Create two functions that should return promise.
+// Create a control machanism within promise to either return resolve or to reject
+// Now,
+//call the first fuction and upon it's resolve(Success execution) trigger/call the second function and print the results.
